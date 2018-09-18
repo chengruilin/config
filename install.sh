9ab8121bdb79301e044789975781785080734e95
qiezi <qiezi@youzan.com>
2018-09-18 20:37:40 +0800

    update install shell

--- install.sh
+++ install.sh
@@ -12,20 +12,30 @@ file_snippets="$HOME/.emacs.d/private/snippets"
 file_bak_snippets="$HOME/.emacs.d/private/snippets.bak1024"
 
 
+
 echo "$current_path"
 echo $file_bak_spacemacs
 
 echo " "
 echo "------------------ spacemacs ------------------"
 
-if [ -h "$file_bak_spacemacs" ]; then
-    echo "bak file exists, delete it"
-    `rm ${file_bak_spacemacs}`
-fi
+#if [ -h "$file_bak_spacemacs" ]; then
+#    echo "bak file exists, delete it"
+#    `rm ${file_bak_spacemacs}`
+#fi
+
+#file_spacemacs="$HOME/_spacemacs"
 
 if [ -f "$file_spacemacs" ] && [ ! -h "$file_spacemacs" ]; then
-    echo "Copy origin .spacemacs file to _spacemacs.bak1024"
-    `mv ${file_spacemacs} ${file_bak_spacemacs}`
+    read -r -p "File .spaceamcs exits, backup it (defualt is _spacemacs.bak1024) ? " check_bak_spacemacs
+
+    if [ ! -n "$check_bak_spacemacs" ]; then
+        check_bak_spacemacs="$file_bak_spacemacs"
+    else
+        check_bak_spacemacs="$HOME/$check_bak_spacemacs"
+    fi
+
+    `mv ${file_spacemacs} ${check_bak_spacemacs}`
 else
     echo "$file_spacemacs file not exists."
 fi
@@ -56,5 +66,29 @@ else
     echo "snippets directory not exists."
 fi
 
+echo " "
+echo "------------------ Org-Files ------------------"
 echo "Link custom snippts to ~/.emacs.d/snippets"
 `ln -sf ${current_path}/snippets/ ${file_snippets}`
+
+#-------------- Git Clone org files --------------
+org_clone_path="$HOME/org"
+cloneOrgFiles() {
+    if command -v git >/dev/null 2>&1; then
+        if [ ! -d "$org_clone_path" ]; then
+            echo 'clone org files to ~/org'
+            `git clone git@github.com:chengruilin/org.git $org_clone_path`
+        fi
+    else
+        echo 'no exists git'
+    fi
+}
+
+read -r -p "Need clone org files? [y/N] " response
+response=${response:="Y"}
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    cloneOrgFiles
+else
+    echo "Finish"
+fi
